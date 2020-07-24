20a6ec7267b5ace8892b528747c2aa79a7ee28de
Anilkumar <devarianil415@gmail.com>
2020-07-24 13:52:18 -0400

    local settings

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ul#1g@+#y##&smt$-e0qye_i3l2pcocuywtot0bqiwa#bnk#w6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['anilkumar13.pythonanywhere.com']
 
 
 # Application definition
@@ -120,7 +120,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATICFILES_DIRS = ( "/portfolio/static", ) 
+
+try:
+    from .local_settings import *
+except ImportError:
+    print("Looks like no local file. you mmust be on production")
